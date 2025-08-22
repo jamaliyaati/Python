b33211046a48ff53a08dd4af02a3468ae8d7bda5
Ebrahim Jamali <jamaliyaati@gmail.com>
2025-08-22 16:12:32 +0500

    :)

--- linear_search.py
+++ linear_search.py
@@ -1,17 +1,16 @@
 List = [15,21,13,16]
-max_index = 4
 
 search = int(input("Enter a number to search: "))
 index = 0
 found = False
 
-while not found and index <= max_index :
-    if search == List[index-1]:
+while not found and index < len(List) :
+    if search == List[index]:
         found = True
-        print("Number found at index: ", index)
+        print("Number found at index: ", index+1)
     else:
         index += 1
 
-if index-1 == max_index and not found:
+if  not found:
     print("Number not found in array")
 
