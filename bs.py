04733ccd1fd2ac84d743c7040129dfb87b12a420
Ebrahim Jamali <jamaliyaati@gmail.com>
2025-08-28 12:48:40 +0500

    :)

---
+++ bs.py
@@ -0,0 +1,23 @@
+List = [1,2,3,4,5,6,7,8,9,10]
+
+item = int(input("Enter a number to search: "))
+
+found=False
+lb = 0
+ub = (len(List)-1)
+mb = 0
+
+while lb<=ub and  found == False :
+    mb = (ub + lb)//2
+    if List[mb] == item:
+        print("Item found at position: ", mb+1)
+        found = True
+    elif List[mb] < item:
+        lb = mb + 1
+    else:
+        ub = mb - 1
+if found == False:
+    print("Item not found")
+
+
+
