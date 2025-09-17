54536055a34c18e8eb281774c09ae781f30a2655
Ebrahim Jamali <jamaliyaati@gmail.com>
2025-09-17 07:46:52 +0500

    update

--- binary_search.py
+++ binary_search.py
@@ -1,23 +1,19 @@
-
-List = [1,2,3,4,5,6,7]
+List = [1, 2, 3, 4, 5, 6, 7]
 item = int(input("Enter a number to search: "))
 
-found=False
+found = False
 lb = 0
-ub = (len(List)-1)
+ub = len(List) - 1
 mb = 0
 
-while lb<=ub and  found == False :
-    mb = (ub + lb)//2
+while lb <= ub and not found:
+    mb = (ub + lb) // 2
     if List[mb] == item:
-        print("Item found at address:",mb+1)
+        print("Item found at address:", mb + 1)
         found = True
     elif List[mb] < item:
         lb = mb + 1
     else:
         ub = mb - 1
-if found == False:
+if not found:
     print("Item not found")
-
-
-
