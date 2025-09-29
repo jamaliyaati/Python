572d7d37373b7f6be3861e3d34387580ac15c13d
Ebrahim Jamali <jamaliyaati@gmail.com>
2025-09-29 17:49:07 +0500

    type

---
+++ type.py
@@ -0,0 +1,13 @@
+number = 5.13
+print(type(number))
+
+number2 = "5"
+print(type(number2)) # used when a function can only return string value
+
+
+
+number2_int = int(number2)
+sum = 5 + number2_int
+print(sum)
+
+
