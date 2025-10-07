0093c4d76531299f4ac28a8b76d7daf8537c4139
Ebrahim Jamali <jamaliyaati@gmail.com>
2025-10-07 23:39:40 +0500

    :)

---
+++ password.py
@@ -0,0 +1,7 @@
+email = input("enter the email: ")
+password = input("enter the password: ")
+
+if password == "12av" and email == "123@google.com":
+    print("correct")
+else:
+    print("incorrect")
