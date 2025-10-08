036a56efba37ce98b107c8779ba5334c1271642a
Ebrahim Jamali <jamaliyaati@gmail.com>
2025-10-08 22:35:02 +0500

    :)

--- input_array.py
+++
@@ -1,6 +0,0 @@
-num = [0,0,0]
-
-for i in range(0,3):
-    num[i] = int(input("Enter number " + str(i+1) + ": "))
-for j in range(0,3):
-    print("Number " + str(j+1) + " is: " + str(num[j]))
--- linear_search.py
+++ linear_search.py
@@ -1,15 +1,7 @@
-List = [15, 21, 13, 16]
+numbers = [15, 21, 13, 16, 18, 20, 10, 10, 50]
 
 search = int(input("Enter a number to search: "))
-index = 0
-found = False
 
-while not found and index < len(List):
-    if search == List[index]:
-        found = True
-        print("Number found at index: ", index + 1)
-    else:
-        index += 1
-
-if not found:
-    print("Number not found in array")
+for x in range(len(numbers)):
+    if numbers[x] == search:
+        print("found at index:",x)
