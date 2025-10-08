ace7ea9b82db2a8e3c062d1881c3626cc35c5e9c
Ebrahim Jamali <jamaliyaati@gmail.com>
2025-10-08 15:54:39 +0500

    :)

--- int_mult.py
+++
@@ -1,6 +0,0 @@
-#DECLARE num : INTEGER
-#DECLARE ans : INTEGER
-
-num = 50
-ans = int((40.45*num))
-print("the integer part  of the answer is:",ans)
---
+++ while_loop.py
@@ -0,0 +1,9 @@
+sum = 0
+count = 0
+number = 0
+while count < 5: #0 1 2 3 4
+    number = float(input("enter a number: "))
+    sum = sum + number
+    count = count + 1
+print("the sum = ",sum)
+
