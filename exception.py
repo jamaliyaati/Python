00eacf191a92d8998de05bb81603aa6ce631e95e
Ebrahim Jamali <jamaliyaati@gmail.com>
2026-01-18 16:36:34 +0500

    :)

---
+++ exception.py
@@ -0,0 +1,30 @@
+try:
+    x = int(input("value1: "))
+    y = int(input("value2: "))
+    
+    z = x//y
+
+    j = len(z)
+
+    
+    print(j)
+
+except NameError:
+        print("variable not defined")
+
+except TypeError:
+        print("wrong data type")
+
+except ZeroDivisionError:
+        print("zero division error")
+
+except ValueError:
+       print("incorrect value")
+
+try:
+    f = open("test.txt", "r")
+    line = f.readline()
+    f.close()
+except FileNotFoundError:
+    print("file not found")
+
